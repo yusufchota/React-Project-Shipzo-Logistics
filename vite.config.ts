8712ee57585ae03dc2a5ec95134e38f1096f7fc7
yusufchota <yusufchota7@gmail.com>
2025-12-01 14:14:18 +0530

    delete old code

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,9 @@
-// import { defineConfig, Plugin } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { createServer } from "./server";
-
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//     fs: {
-//       allow: [".", "./client", "./shared"],
-//       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-//     },
-//   },
-//   build: {
-//     outDir: "dist/spa",
-//   },
-//    base: mode === "production" ? "/Shipzo-Logistics/" : "/",
-//   plugins: [react(), expressPlugin()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./client"),
-//       "@shared": path.resolve(__dirname, "./shared"),
-//     },
-//   },
-// }));
-
-// function expressPlugin(): Plugin {
-//   return {
-//     name: "express-plugin",
-//     apply: "serve", // Only apply during development (serve mode)
-//     configureServer(server) {
-//       const app = createServer();
-
-//       // Add Express app as middleware to Vite dev server
-//       server.middlewares.use(app);
-//     },
-//   };
-// }
-
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { createServer } from "./server";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -52,15 +13,11 @@ export default defineConfig(({ mode }) => ({
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
     },
   },
-
   build: {
     outDir: "dist/spa",
   },
-
-  base: mode === "production" ? "/Shipzo-Logistics/" : "/",
-
-  plugins: [react()],
-
+   base: mode === "production" ? "/Shipzo-Logistics/" : "/",
+  plugins: [react(), expressPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
@@ -68,3 +25,16 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
+function expressPlugin(): Plugin {
+  return {
+    name: "express-plugin",
+    apply: "serve", // Only apply during development (serve mode)
+    configureServer(server) {
+      const app = createServer();
+
+      // Add Express app as middleware to Vite dev server
+      server.middlewares.use(app);
+    },
+  };
+}
